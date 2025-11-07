4c3421c309bf2f7343db4a2dc1f2ef9c1b9c1d49
kenyacrawford19-cpu <kenyacrawford19@gmail.com>
2025-11-06 21:59:38 -0500

    enter data in main class

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,4 +1,22 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        boolean running = true;
+
+        while (running) {
+            System.out.println("============");
+            System.out.println("Welcome to Rolling Slices Pizza");
+            System.out.println("============");
+            System.out.println("1) New Order");
+            System.out.println("0) Exit");
+            System.out.println("Enter choice: ");
+
+            String input = scanner.nextLine();
+        }
+    }
 }
--- src/main/java/com/pluralsight/Menu.java
+++ src/main/java/com/pluralsight/Menu.java
@@ -1,4 +1,10 @@
 package com.pluralsight;
 
+import java.util.List;
+
 public class Menu {
+    public static final List<Toppings> meatToppings = List.of(
+       new Toppings("Pepperoni" , true)
+
+    );
 }
