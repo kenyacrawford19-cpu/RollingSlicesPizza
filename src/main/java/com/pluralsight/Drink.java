1db413ab606655b6a3b3fbc466e086b001197f7a
kenyacrawford19-cpu <kenyacrawford19@gmail.com>
2025-11-06 21:09:25 -0500

    added code in Order class

--- src/main/java/com/pluralsight/Drink.java
+++ src/main/java/com/pluralsight/Drink.java
@@ -1,5 +1,18 @@
 package com.pluralsight;
 
 public class Drink {
+    private String size;
+    private String flavor;
 
+    public Drink(String size, String flavor) {
+        this.size = size;
+        this.flavor = flavor;
+    }
+    public double getCost() {
+        return switch (size) {
+            case Small -> 1.50;
+            case Medium -> 2.00;
+            case Large -> 2.50;
+        };
+    }
 }
--- src/main/java/com/pluralsight/Order.java
+++ src/main/java/com/pluralsight/Order.java
@@ -1,4 +1,30 @@
 package com.pluralsight;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Order {
-}
+    private List<Pizza> pizzas = new ArrayList<>();
+    private List<Drink> drinks = new ArrayList<>();
+    private int chesseSticks = 0;
+
+    public void addPizza(Pizza p) {pizzas.add(p); }
+    public void addDrink(Drink d) {drinks.add(d); }
+    public void addcheeseSticks() {chesseSticks++; }
+
+    public void displayNewestFirst() {
+        for (int i = pizzas.size() - 1; i >=0; i--)
+            System.out.println("Pizza: " + pizzas.get(i));
+        for (int i = drinks.size() - 1; i >=0; i--)
+            System.out.println("Drink: " + drinks.get(i));
+        if (chesseSticks > 0 ) System.out.println("Cheese Sticks: " + chesseSticks);
+    }
+    public double getTotal() {
+        double total = 0;
+        for (Pizza p : pizzas) total += p.getCost();
+        for (Drink d : drinks) total += d.getCost();
+        total += chesseSticks * 5.00;
+        return total;
+        }
+    }
+
